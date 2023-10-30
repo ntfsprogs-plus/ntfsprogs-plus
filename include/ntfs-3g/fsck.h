3f55d6e1a461fb2365cd3b9b298f5fe7f1ccf526
JaeHoon Sim <jay.sim@lge.com>
2023-10-30 17:12:52 +0900

    ntfsck: add fsck.c/fsck.h in libntfs-3g
    First step for checking bitmap duplication.
    Separate some functions related with fsck bitmap from ntfsck.c
    Add new file fsck.c/fsck.h in library directory to call in library.
    
    Signed-off-by: JaeHoon Sim <jay.sim@lge.com>

---
+++ include/ntfs-3g/fsck.h
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier : GPL-2.0 */
+#ifndef _NTFS_FSCK_H
+#define _NTFS_FSCK_H
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef HAVE_STDIO_H
+#include <stdio.h>
+#endif
+#ifdef HAVE_SYS_PARAM_H
+#include <sys/param.h>
+#endif
+	/* Do not #include <sys/mount.h> here : conflicts with <linux/fs.h> */
+#ifdef HAVE_MNTENT_H
+#include <mntent.h>
+#endif
+
+#include "volume.h"
+
+#define NTFS_BUF_SIZE_BITS		(13)
+#define NTFSCK_BYTE_TO_BITS		(3)
+#define NTFSCK_BM_BITS_SIZE	(NTFS_BUF_SIZE << 3)
+#define FB_ROUND_UP(x)		(((x) + ((NTFS_BUF_SIZE << 3) - 1)) & ~((NTFS_BUF_SIZE << 3) - 1))
+#define FB_ROUND_DOWN(x)	(((x) & ~(NTFS_BUF_SIZE - 1)) >> NTFS_BUF_SIZE_BITS)
+
+int ntfs_fsck_set_mftbmp_value(ntfs_volume *vol, u64 mft_no, int value);
+char ntfs_fsck_mftbmp_get(ntfs_volume *vol, const u64 bit);
+int ntfs_fsck_mftbmp_clear(ntfs_volume *vol, u64 mft_no);
+int ntfs_fsck_mftbmp_set(ntfs_volume *vol, u64 mft_no);
+void ntfs_fsck_set_bitmap_range(u8 *bm, s64 pos, s64 length, u8 bit);
+u8 *ntfs_fsck_get_lcnbmp_block(ntfs_volume *vol, s64 pos);
+
+ntfs_volume *ntfs_fsck_mount(const char *path __attribute__((unused)),
+		ntfs_mount_flags flags __attribute__((unused)));
+void ntfs_fsck_umount(ntfs_volume *vol);
+#endif /* _NTFS_FSCK_H */
--- include/ntfs-3g/volume.h
+++ include/ntfs-3g/volume.h
@@ -347,7 +347,13 @@ struct _ntfs_volume {
 				   efs-encrypted files */
 	ntfs_volume_special_files special_files; /* Implementation of special files */
 	const char *abs_mnt_point; /* Mount point */
+
 	u64 lost_found;		/* mft record number for lost_found directory */
+	u8 **fsck_lcn_bitmap;
+	u64 max_flb_cnt;
+	u8 **fsck_mft_bitmap;
+	u64 max_fmb_cnt;
+
 #ifdef XATTR_MAPPINGS
 	struct XATTRMAPPING *xattr_mapping;
 #endif /* XATTR_MAPPINGS */
